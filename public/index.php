b9a5354c72ef0e934654775989a78c27d02417e0
Anthony De La Rosa <anthony.delarosa@canadadrives.ca>
2022-02-14 11:54:10 -0800

    adding index.php changes

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'Hello World!';
+$realIP = file_get_contents("http://ipecho.net/plain");
+
+echo "
+    <div style=\"text-align: center; font-size: 50px; color: #006400; font-weight: bold\">
+        Hey Friends! <br /> This is VPN Public IP: {$realIP}!
+    </div>
+";
